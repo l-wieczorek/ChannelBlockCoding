c53fba98c2eec93330246bc1a78738d318fa43b8
uarmes <bartlomiej.semrau@gmail.com>
2018-11-14 21:17:22 +0100

    kanal.hpp modified

--- KKB/KKB/Kanal.hpp
+++ KKB/KKB/Kanal.hpp
@@ -1,5 +1,2 @@
-#ifndef Kanal_hpp
-#define Kanal_hpp
-	float gauss(float mean, float sigma);
-	void kanal(float es_n0, long dl_kan, int *wej, float *wyj);
-#endif
+float gauss(float mean, float sigma);
+void kanal(float es_n0, long dl_kan, int *wej, float *wyj);
